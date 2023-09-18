fc10fe8d30c1f6c49e3dfe9fa8795280ebba482a
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-09-17 21:49:55 -0400

    added questions to questions.js file to run on webpage

---
+++ js/questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        questionText: 'What does JS stand for?',
+        choices: ['Juststrings', 'Javascript', 'Juicesauce', 'Justspiders'],
+        correctAnswer_choicesIndex: 1,
+    },
+    {
+        questionText: 'Inside which HTML element do we put the JavaScript?',
+        choices: ['<javascript>', '<body>', '<script>', '<footer>'],
+        correctAnswer_choicesIndex: 2,
+    },
+    {
+        questionText: 'How does a FOR loop start?',
+        choices: ['for (i =0; i <=5; i++)', 'for (i =0; i <=5)', 'for i =0 to 5', 'for (i <= 5;i++)'],
+        correctAnswer_choicesIndex: 0,
+    },
+    {
+        questionText:
+            'How do you write "Hello World" in an alert box?',
+        choices: ['alert("Hello World");', 'alertBox("Hello World");', 'msg("Hello World");', 'prompt("Hello World");'],
+        correctAnswer_choicesIndex: 0,
+    },
+    {
+        questionText:
+            'How do you call a function named "myFunction"?',
+        choices: ['call myFunction()', 'call function myFunction()', 'MyFunction;', 'myFunction();'],
+        correctAnswer_choicesIndex: 3,
+    },
+];
