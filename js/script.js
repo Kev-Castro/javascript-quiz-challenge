0409d8716cb8cf5b527b9964e62d960977920e3a
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-09-17 21:46:36 -0400

    added JS code in script.js file to run on webpage

---
+++ js/script.js
@@ -0,0 +1,94 @@
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        countdown_timer.textContent = secondsLeft;
+        if (secondsLeft === 0 || questionsIndex > 4) {
+            clearInterval(timerInterval);
+            gameOver();
+        }
+    }, 1000)
+};
+
+
+function startQuiz() {
+    if (questionsIndex < 5) {
+        main_el.innerHTML = '';
+
+
+        var section2 = document.createElement('section');
+        section2.id = 'question_section';
+        section2.classList.add('section_center');
+        main_el.appendChild(section2);
+
+
+        var question_text = document.createElement('h1');
+        question_text.id = 'question_text'
+        question_text.textContent = questions[questionsIndex].questionText;
+
+
+        section2.appendChild(question_text);
+
+
+        for (var i = 0; i < listEl.length; i++) {
+            listEl[i].textContent = questions[questionsIndex].choices[i];
+            section2.appendChild(listEl[i]);
+        }
+        listEl[questions[questionsIndex].correctAnswer_choicesIndex].id = 'correct_answer';
+    } else {
+        gameOver();
+    };
+
+
+};
+li1.addEventListener('click', function () {
+    if (li1.id !== 'correct_answer') {
+        secondsLeft -= 10;
+        questionsIndex++;
+        startQuiz();
+    } else {
+        currentScore = currentScore + 10;
+        questionsIndex++;
+        startQuiz();
+    }
+})
+li2.addEventListener('click', function () {
+    if (li2.id !== 'correct_answer') {
+        secondsLeft -= 10;
+        questionsIndex++;
+        startQuiz();
+    } else {
+        currentScore = currentScore + 10;
+        questionsIndex++;
+        startQuiz();
+    }
+})
+li3.addEventListener('click', function () {
+    if (li3.id !== 'correct_answer') {
+        secondsLeft -= 10;
+
+
+        questionsIndex++;
+        startQuiz();
+    } else {
+        currentScore = currentScore + 10;
+        questionsIndex++;
+        startQuiz();
+    }
+})
+li4.addEventListener('click', function () {
+    if (li4.id !== 'correct_answer') {
+        secondsLeft -= 10;
+        questionsIndex++;
+        startQuiz();
+    } else {
+        currentScore = currentScore + 10;
+        questionsIndex++;
+        startQuiz();
+    }
+})
+
+
+intro_start_button.addEventListener('click', function () {
+    setTime();
+    startQuiz();
+});
